7009dd08377541077e207ca849b043d53b190712
JHONTONCONI <btaedwin123@gmail.com>
2025-05-28 17:49:26 -0400

    MODIFICACIÓN

--- primer-ejerciciorepetitivas.cpp
+++ primer-ejerciciorepetitivas.cpp
@@ -4,11 +4,11 @@ using namespace std;
 
 int main (){
 	int cantidadmultiplos, i, mul;
-	cout<<nn *************************************************<<endl;
-	cout<< INGRESE LA CANTIDAD DE MULTIPLOS QUE DESEA CONOCER<<endl;
-	cout<< *************************************************<<endl;
+	cout<<"\n\n *************************************************"<<endl;
+	cout<< "INGRESE LA CANTIDAD DE MULTIPLOS QUE DESEA CONOCER"<<endl;
+	cout<<" *************************************************"<<endl;
 	cin>>cantidadmultiplos;
-	cout<<LOS PRIMEROS <<cantidadmultiplos<< MULTIPLOS DE 7 SON: <<endl;
+	cout<<"LOS PRIMEROS "<<cantidadmultiplos<< "MULTIPLOS DE 7 SON:" <<endl;
 	for (i=0; i<cantidadmultiplos; i=i+1){
 		mul=7*i;
 		cout<<mul<<endl;
