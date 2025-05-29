a77bd5432f4a0208d4b97a50eabb9d9edb9c9fbf
JHONTONCONI <btaedwin123@gmail.com>
2025-05-29 12:14:33 -0400

    AGREGUE EL TERCER EJERCICIO

---
+++ tercer_ejerciciorepetitivas.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+	int cantidadnum, i, s;
+	cout<<nn ******************************<<endl;
+	cout<< INGRESE LA CANTIDAD DE NUMEROS<<endl;
+	cout<< ******************************<<endl;
+	cin>>cantidadnum;
+	s=0;
+	for(i=1; i<=cantidadnum; i++){
+		s=s+i;
+	}
+	cout<<LA SUMA DE LOS PRIMEROS NUMEROS NATURALES ES: <<s<<endl;
+}
