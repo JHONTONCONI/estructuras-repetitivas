6b442104622280304ad60c9ebc63ecd7ad240486
JHONTONCONI <btaedwin123@gmail.com>
2025-05-30 09:40:22 -0400

    MEJORE LA INTERFAZ

--- sexto_ejerciciorepetitivas.cpp
+++ sexto_ejerciciorepetitivas.cpp
@@ -3,20 +3,30 @@
 using namespace std;
 
 int main(){
-	double num, promedio, i, suma, numero;
-	cout<<INGRESE LA CANTIDAD DE NUMEROS<<endl;
-	cin>>num;
-	suma=0;
-	for(i=1; i<=num; i++){
-		cout<<INGRESE UN NUMERO<<endl;
+	double cantidadnumeros, promedio, i, suma, numero;
+	cout<<"\n\n     ******************************"<<endl;
+	cout<<"     INGRESE LA CANTIDAD DE NUMEROS"<<endl;
+	cout<<"     ******************************"<<endl;
+	cout<<endl;
+	cin>>cantidadnumeros;
+	cout<<endl;
+	suma = 0;
+	for(i = 1; i <= cantidadnumeros; i++){
+		cout<<"\n\n     *****************"<<endl;
+		cout<<"     INGRESE UN NUMERO"<<endl;
+		cout<<"     *****************"<<endl;
+		cout<<endl;
 		cin>>numero;
 		if (numero>0){
 			suma=suma+numero;
 		}else{
-			cout<<INGRESE UN NUMERO POSITIVO<<endl;
+			cout<<"\n\n     **************************"<<endl;
+			cout<<"     INGRESE UN NUMERO POSITIVO"<<endl;
+			cout<<"     **************************"<<endl;
+			cout<<endl;
 			i=i-1;
 		}
 	}
-	promedio=suma/num;
-	cout<<EL PROMEDIO ES: << promedio <<endl;
+	promedio=suma/cantidadnumeros;
+	cout<<"EL PROMEDIO ES: "<< promedio <<endl;
 }
