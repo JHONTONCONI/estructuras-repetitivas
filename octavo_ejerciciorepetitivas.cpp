9461d843c1e54b4f8afdf903ebde6e0a630fe3cf
JHONTONCONI <btaedwin123@gmail.com>
2025-06-01 16:08:07 -0400

    MEJORE LA INTERFAZ DEL EJERCICIO

--- octavo_ejerciciorepetitivas.cpp
+++ octavo_ejerciciorepetitivas.cpp
@@ -3,16 +3,25 @@
 using namespace std;
 
 int main(){
-	int num, n, m, i;
-	cout<<INGRESE LA CANTIDAD DE NUMERO<<endl;
-	cin>>n;
-	m=0;
-	for(i=1;i<=n;i++){
-		cout<<INGRESE UN NUMERO<<endl;
-		cin>>num;
-		if (num>m){
-			m=num;
+	int numero, cantidadnumeros, mayor, i;
+	cout<<"\n\n     *****************************"<<endl;
+	cout<<"     INGRESE LA CANTIDAD DE NUMERO"<<endl;
+	cout<<"     *****************************"<<endl;
+	cin>>cantidadnumeros;
+	
+	for(i=1;i<=cantidadnumeros;i++){
+		cout<<"\n\n     *****************"<<endl;
+		cout<<"     INGRESE UN NUMERO"<<endl;
+		cout<<"     ****************"<<endl;
+		cin>>numero;
+		if (i==1){
+			mayor=numero;
+		}
+		if (numero>mayor){
+			mayor=numero;
 		}
 	}
-	cout<<EL MAYOR NUMERO ES: << m<<endl;
+	cout<<"\n\n     *********************"<<endl;
+	cout<<"     EL MAYOR NUMERO ES: "<<mayor<<endl;
+	cout<<"     *********************"<<endl;
 }
