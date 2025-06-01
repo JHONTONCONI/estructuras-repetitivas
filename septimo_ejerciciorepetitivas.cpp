d6aebbe0c7f59c0c28ef9992484feb0c4290e2db
JHONTONCONI <btaedwin123@gmail.com>
2025-05-31 22:05:21 -0400

    MEJORE LA INTERFAZ DEL EJERCICIO

--- septimo_ejerciciorepetitivas.cpp
+++ septimo_ejerciciorepetitivas.cpp
@@ -5,14 +5,18 @@ using namespace std;
 int main(){
 	double n, promediopar, promedioimpar, i, sumapar, sumaimpar, contadorpar, contadorimpar;
 	int num;
-	cout<<INGRESE LA CANTIDAD DE NUMEROS<<endl;
+	cout<<"\n\n     ******************************"<<endl;
+	cout<<"     INGRESE LA CANTIDAD DE NUMEROS"<<endl;
+	cout<<"     ******************************"<<endl;
 	cin>>n;
 	contadorpar=0;
 	contadorimpar=0;
 	sumapar=0;
 	sumaimpar=0;
 	for(i=1; i<=n; i++){
-		cout<<INGRESE UN NUMERO<<endl;
+		cout<<"\n\n     *****************"<<endl;
+		cout<<"     INGRESE UN NUMERO"<<endl;
+		cout<<"     *****************"<<endl;
 		cin>>num;
 		if(num%2==0){
 			contadorpar=contadorpar+1;
@@ -24,7 +28,11 @@ int main(){
 	}
 	promediopar=sumapar/contadorpar;
 	promedioimpar=sumaimpar/contadorimpar;
-	cout<<EL PROMEDIO DE LOS PARES ES: <<promediopar<<endl;
-	cout<<EL PROMEDIO DE LOS IMPARES ES: <<promedioimpar<<endl;
+	cout<<"\n\n     ******************************"<<endl;
+	cout<<"     EL PROMEDIO DE LOS PARES ES: "<<promediopar<<endl;
+	cout<<"     ******************************"<<endl;
+	cout<<"\n\n     ********************************"<<endl;
+	cout<<"     EL PROMEDIO DE LOS IMPARES ES: "<<promedioimpar<<endl;
+	cout<<"     ********************************"<<endl;
 	
 }
